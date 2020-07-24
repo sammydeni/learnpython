3865dd5f3ac7605fc706260b429f98d72c606442
sammydeni <ri7g6fh6>
2020-07-24 18:27:51 +0200

    up

--- auto_airbnb.py
+++ auto_airbnb.py
@@ -1,23 +1,22 @@
 import time
 from selenium import webdriver
 driver = webdriver.Chrome()
-#driver = webdriver.Chrome("C:\bin\chromedriver")
 
-#Apre il sito
+#open the website
 driver.get('https://www.airbnb.it/')
-time.sleep(3)
+time.sleep(2)
 
-#Cerca il luogo
+#search the place
 search = driver.find_element_by_xpath('//*[@id="bigsearch-query-attached-query"]')
 search.send_keys('Lido di Pomposa')
-time.sleep(3)
+time.sleep(1.5)
 
 #accept cookie
 button = driver.find_element_by_xpath('//*[@id="main-cookies-banner-container"]/div[2]/div[4]/div[2]/div/button')
 button.click()
 time.sleep(1)
 
-#inserimento date
+#insert date
 button = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[1]/div[3]/div[1]/button/div/div[2]')
 button.click()
 time.sleep(1)
@@ -25,14 +24,29 @@ time.sleep(1)
 #skip month
 button = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[1]/div[3]/div[4]/section/div/div/div/div/div/div[2]/div[1]/div[2]/div/button')
 button.click()
-time.sleep(2)
+time.sleep(1)
 
-#seleziono il giorno iniziale
+#select first day
 button = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[1]/div[3]/div[4]/section/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[4]/td[6]/div/div')
 button.click()
-time.sleep(2)
+time.sleep(1)
 
-#seleziono il giorno finale
+#select final day
 button = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[1]/div[3]/div[4]/section/div/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[6]/td[1]/div/div')
 button.click()
-time.sleep(2)
+time.sleep(1)
+
+#add guests
+button = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[1]/div[5]/button/div/div[2]')
+button.click()
+time.sleep(1)
+i=0
+while i<=7:
+    button = driver.find_element_by_xpath('//*[@id="stepper-adults"]/button[2]')
+    button.click()
+    time.sleep(0.3)
+    i +=1
+
+#click the search button
+button = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[2]/button/span[1]/span')
+button.click()
